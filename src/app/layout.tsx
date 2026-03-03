dd8775d8d051fdb9f5032fd646043bcbaaa90500
Nasuschnyi <91079054+Nasuschnyi@users.noreply.github.com>
2026-03-03 15:52:16 +0100

    add-foreground-app

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,18 +1,14 @@
-import type { Metadata } from 'next';
-import { Geist, Geist_Mono } from 'next/font/google';
-import './globals.css';
+import { Unbounded } from 'next/font/google';
+import './globals.scss';
 
-const geistSans = Geist({
-	variable: '--font-geist-sans',
+const unbounded = Unbounded({
 	subsets: ['latin'],
+	weight: ['300', '400', '500'],
+	variable: '--font-unbounded',
+	display: 'swap',
 });
 
-const geistMono = Geist_Mono({
-	variable: '--font-geist-mono',
-	subsets: ['latin'],
-});
-
-export const metadata: Metadata = {
+export const metadata = {
 	title: 'Cars Classic Autotrader',
 	description: 'Classic cars marketplace',
 };
@@ -23,10 +19,11 @@ export default function RootLayout({
 	children: React.ReactNode;
 }>) {
 	return (
-		<html lang="en">
-			<body className={`${geistSans.variable} ${geistMono.variable}`}>
-				{children}
-			</body>
+		<html
+			lang="en"
+			className={unbounded.variable}
+		>
+			<body>{children}</body>
 		</html>
 	);
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,8 @@
+export const metadata = {
+	title: 'Cars Classic Autotrader | Classic Cars Marketplace',
+	description: 'Discover and explore timeless classic retro cars.',
+};
+
 export default function Home() {
 	return <h1>Retro cars 1960-1970</h1>;
 }
